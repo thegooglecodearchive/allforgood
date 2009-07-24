85c139892c651afb25227696f62961111b3c5193
LeonPalm <LeonPalm@3538cd10-6cae-11de-9a7c-6184c298a20a>
2009-07-24 15:05:45 +0000

    Added code to update the SOLR index in the pipeline (coming soon: integration)

--- trunk/datahub/footprint_lib.py
+++ trunk/datahub/footprint_lib.py
@@ -21,6 +21,7 @@ import gzip
 import hashlib
 import urllib
 import re
+from csv import DictReader, DictWriter, excel_tab, register_dialect, QUOTE_NONE
 from datetime import datetime
 import geocoder
 import parse_footprint
@@ -159,6 +160,11 @@ FIELDTYPES = {
   "location_string":"string",
   "orgLocation":"string",
 }
+
+class our_dialect(excel_tab):
+  quotechar = ''
+  quoting = QUOTE_NONE
+register_dialect('our-dialect', our_dialect)
 
 def print_progress(msg, filename="", progress=None):
   """print progress indicator."""
@@ -912,6 +918,68 @@ def ftp_to_base(filename, ftpinfo, instr):
     print_progress("giving up.")
   ftp.quit()
   data_fh.close()
+  
+def solr_retransform(fname):
+  print fname
+  f = open(fname, "r")
+  csv_reader = DictReader(f, dialect='our-dialect')
+  csv_reader.next()
+  fnames = csv_reader.fieldnames[:]
+  fnames.append("c:eventrangeend:datetime")
+  fnames.append("c:eventrangestart:datetime")
+  fnamesdict = dict([(x, x) for x in fnames])
+  f = open(fname, "r")
+  csv_reader = DictReader(f, dialect='our-dialect')
+  csv_writer = DictWriter(open (fname + '.transformed', 'w'),
+                          dialect='excel-tab',
+                          fieldnames=fnames)
+  csv_writer.writerow(fnamesdict)
+  for rows in csv_reader:
+    for key in rows.keys():
+      if key.find(':dateTime') != -1:
+        rows[key] += 'Z'
+      elif key.find(':integer') != -1:
+        if rows[key] == '':
+          rows[key] = 0
+        else:
+          rows[key] = int(rows[key])
+      
+    # Split the date range into separate fields
+    # event_date_range can be either start_date or start_date/end_date
+    split_date_range = rows["event_date_range"].split('/')
+    rows["c:eventrangeend:datetime"] = split_date_range[0]
+    if len(split_date_range) > 1:
+      rows["c:eventrangestart:datetime"] = split_date_range[1]
+    
+    # Fix to the +1000 to lat/long hack   
+    if not rows['c:latitude:float'] is None:
+      rows['c:latitude:float'] = float(rows['c:latitude:float']) - 1000.0
+    if not rows['c:longitude:float'] is None:
+      rows['c:longitude:float'] = float(rows['c:longitude:float']) - 1000.0
+      
+    csv_writer.writerow(rows)
+  f.close()
+  
+# TODO: add a choice of backend URL
+def update_solr_index(filename):
+  in_fname = filename + '.gz'
+  out_fname = filename + '.transformed'
+  
+  f_out = open(out_fname, 'wb')
+  f_in = gzip.open(in_fname, 'rb')
+  
+  f_out.writelines(f_in)
+  f_out.close()
+  f_in.close()
+  
+  solr_retransform(out_fname)
+  
+  backend_url = 'http://google2.osuosl.org:8080/solrdev/'  
+  cmd = 'curl \'' + backend_url + \
+  'update/csv?commit=true&separator=%09&escape=%10\' --data-binary ' + \
+   out_fname + \
+   ' -H \'Content-type:text/plain; charset=utf-8\';'
+  subprocess.call(cmd, shell=True)
 
 def guess_parse_func(inputfmt, filename):
   """from the filename and the --inputfmt,guess the input type and parse func"""
