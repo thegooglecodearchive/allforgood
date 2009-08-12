dd12f6d38107f861b23dcd8ed775f378f9f90ed6
ehysen <ehysen@3538cd10-6cae-11de-9a7c-6184c298a20a>
2009-08-12 21:29:42 +0000

    Delete old 1.7 branch

--- branches/release_1_7_old/frontend/local_keys.py
+++
@@ -1,6 +0,0 @@
-MAPS_API_KEYS = {}
-
-MAPS_API_KEYS['www.adamsah.net:8080'] = 'ABQIAAAAxq97AW0x5_CNgn6-nLxSrxQPiECf40c9sk8_oYaM1tejJgt_DBQGX9FrJhDYEm_Q_8aqbVKUzollqg'
-MAPS_API_KEYS['www.adamsah.net:8081'] = 'ABQIAAAAxq97AW0x5_CNgn6-nLxSrxSatViGQnF70MoboVDRRVzoLj4T8hTGWmAjUKagGrnWr-xTwSWv4XFuiw'
-MAPS_API_KEYS['www.adamsah.net:8082'] = 'ABQIAAAAxq97AW0x5_CNgn6-nLxSrxTGEVewtGazShFSG9KX3KfJ-OzRuxRYmck9mME2a1DVHyoL1GbqprKLeA'
-MAPS_API_KEYS['www.adamsah.net:8083'] = 'ABQIAAAAxq97AW0x5_CNgn6-nLxSrxTaJ1NpVtTebRGVkFgIOxwdBr6gvhSK1BMuNuwwydj3shBNvtPyShE9CA'
