6c4ff70f4fa177d65ec2ebfa904aa753f97a5981
adam.sah <adam.sah@3538cd10-6cae-11de-9a7c-6184c298a20a>
2009-09-09 20:21:09 +0000

    added universalgiving FPXML feed refreshed geocode cache

--- trunk/datahub/footprint_lib.py
+++ trunk/datahub/footprint_lib.py
@@ -896,6 +896,8 @@ def guess_shortname(filename):
     return "citizencorps"
   if re.search(r'ymca', filename):
     return "ymca"
+  if re.search(r'universalgiving', filename):
+    return "universalgiving"
   if re.search(r'washoe', filename):
     return "washoecounty"
   if re.search("habitat", filename):
@@ -1011,6 +1013,10 @@ def guess_parse_func(inputfmt, filename):
     return "fpxml", fp.parser(
       '129', 'washoecounty', 'washoecounty', 'http://www.washoecounty.us/',
       'Washoe County, Nevada')
+  if shortname == "universalgiving":
+    return "fpxml", fp.parser(
+      '130', 'universalgiving', 'universalgiving', 'http://www.universalgiving.org/',
+      'Universal Giving')
 
   if shortname == "habitat":
     parser = fp.parser(
