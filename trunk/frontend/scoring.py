c4058199894e235d8c4fcdbb1e18381a24cf4b73
adam.sah <adam.sah@3538cd10-6cae-11de-9a7c-6184c298a20a>
2009-11-15 18:39:42 +0000

    add boost for events that started recently (vs. far in the past)

--- trunk/frontend/scoring.py
+++ trunk/frontend/scoring.py
@@ -63,13 +63,13 @@ def score_results_set(result_set, args):
     MAXTIME = 500.0 * ONEDAY
     start_delta = res.startdate - datetime.now()
     start_delta_secs = start_delta.days*ONEDAY + start_delta.seconds
-    start_delta_secs = min(max(start_delta_secs, 0), MAXTIME)
+    start_delta_secs = min(abs(start_delta_secs), MAXTIME)
     end_delta = res.enddate - datetime.now()
     end_delta_secs = end_delta.days*ONEDAY + end_delta.seconds
     end_delta_secs = min(max(end_delta_secs, start_delta_secs), MAXTIME)
     date_dist_multiplier = 1
     if end_delta_secs <= 0:
-      date_dist_multiplier = .0001
+      date_dist_multiplier = .000001
     if start_delta_secs > 0:
       # further out start date = lower rank (roughly 1/numdays)
       date_dist_multiplier = 1.0/(start_delta_secs/ONEDAY)
