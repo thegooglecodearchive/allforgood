c0caa333e3343e05a743853376ef954cf7f9af7e
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-01-16 19:06:38 +0000

    pipeline script

---
+++ branches/release_2_0/datahub/lock_pipeline.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+:
+#set -x
+
+DIR=/home/footprint/allforgood-read-only/datahub
+DASHBOARD_DIR=/home/footprint/public_html/dashboard
+
+YMD0=`date +%Y%m%d`
+FINGERPRINT="pipeline.py -s"
+COUNT=`ps -ef | grep "$FINGERPRINT" | grep -v grep | wc -l`
+
+if [ $COUNT -lt 1 ]
+then
+	# change to our directory
+	cd $DIR
+	if [ $? -ne 0 ]
+	then
+		./notify_michael.sh cd failed
+		exit 1
+	else
+		./notify_michael.sh pipeline started
+	fi
+
+	if [ "$*" = "" ]
+	then
+		./notify_michael.sh download started 
+		./download.sh
+		./notify_michael.sh download complete 
+	fi
+
+	# clear list of duplicated opps
+	rm -fr dups; mkdir dups
+	if [ $? -ne 0 ]
+	then
+		./notify_michael.sh mkdir dups failed
+		exit 1
+	fi
+
+	./notify_michael.sh pipeline processing
+	./pipeline.py -s $*
+
+        if [ $? -ne 0 ]
+        then
+                ./notify_michael.sh pipeline FAIL
+                exit 1
+        fi
+
+	./notify_michael.sh pipeline uploading
+
+        # upload the opps to solr
+	if [ "$*" = "" ]
+	then
+		./upload.sh
+	else
+		for IT in $*
+		do
+        		./upload.sh `ls -1 $IT*.transformed`
+		done
+	fi
+	./notify_michael.sh pipeline upload complete
+
+        # update the dash board
+        YMD=`date +%Y%m%d`
+        if [ "$YMD" = "$YMD0" -a "$*" = "" ]
+        then
+                # but only if we haven't lapped
+                cp dashboard.ing/* $DASHBOARD_DIR
+        fi
+
+	./notify_michael.sh pipeline stopped
+else
+	echo -n "pipline lapped " ; date
+	./notify_michael.sh pipeline lapped
+fi
