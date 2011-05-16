7d6383cda07689179da719cc7e767cda1cf976ca
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-05-16 13:26:25 +0000

    v3

--- branches/release_2_0.transitional/frontend/main.py
+++ branches/release_2_0.transitional/frontend/main.py
@@ -21,6 +21,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import logging
