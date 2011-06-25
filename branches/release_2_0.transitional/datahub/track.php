bf1da544a0317080cc8f11f016052dbf44cafcb7
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-06-25 17:59:34 +0000

    corrected GA issue

---
+++ branches/release_2_0.transitional/datahub/track.php
@@ -0,0 +1,63 @@
+<?php
+  ini_set("allow_url_fopen", "1");
+  if (empty($category)) {
+    $category = $_GET["category"];
+  }
+  if (empty($action)) {
+    $action = $_GET["action"];
+  }
+  if (empty($label)) {
+    $label = $_GET["label"];
+  }
+  $logfile = "log/" . date("Ymd") . ".txt";
+  if (($fh = fopen($logfile, 'a'))) {
+    $when = date("Y-m-d\tH:i:s");
+    fwrite($fh, "$when\t$category\t$action\t$label\n");
+    fclose($fh);
+  }
+  
+
+  #$GA_DOMAIN = "http://" . $_SERVER["SERVER_NAME"];
+  $GA_DOMAIN = "http://www.allforgood.org";
+  // Copyright 2009 Google Inc. All Rights Reserved.
+  #$GA_ACCOUNT = "GA-8689219-2";
+  $GA_ACCOUNT = "MO-8689219-2";
+  $GA_PIXEL = "http://li169-139.members.linode.com/~footprint/ga.php";
+
+  function googleAnalyticsGetImageUrl() {
+    global $GA_ACCOUNT, $GA_PIXEL;
+    global $category, $action, $label;
+    $url = "";
+    $url .= $GA_PIXEL . "?";
+    $url .= "utmac=" . $GA_ACCOUNT;
+    $url .= "&utmn=" . rand(0, 0x7fffffff);
+
+    $referer = $_SERVER["HTTP_REFERER"];
+    $query = $_SERVER["QUERY_STRING"];
+    $path = $_SERVER["REQUEST_URI"];
+
+    if (empty($referer)) {
+      $referer = "-";
+    }
+    $url .= "&utmr=" . urlencode($referer);
+
+    if (!empty($path)) {
+      $url .= "&utmp=" . urlencode($path);
+    }
+
+    $url .= "&guid=ON";
+    $url .= "&category=$category";
+    $url .= "&action=$action";
+    $url .= "&label=$label";
+    #$url .= "&utmdebug=1";
+
+    return str_replace("&", "&amp;", $url);
+  }
+
+  $googleAnalyticsImageUrl = googleAnalyticsGetImageUrl();
+  #echo '<img src="' . $googleAnalyticsImageUrl . '" />';
+  #echo "$googleAnalyticsImageUrl";
+  
+  header("Content-Type: image/gif");
+  readfile($googleAnalyticsImageUrl);
+?>
