6a8c01040b230e42cb87fb0a3af6e1b17bef051b
danstryker1@gmail.com <danstryker1@gmail.com@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-07-19 19:38:29 +0000

    Delete trunk

--- trunk/frontend/facebookapp/fb_model.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import db
-
-class EventsData (db.Model):
-  date_added = db.DateTimeProperty(auto_now_add=True)
-  afg_story_id = db.StringProperty()
-  fb_event_id = db.StringProperty()
-  fb_user_id = db.StringProperty()
-  fb_network_id = db.StringProperty(default='')
-
