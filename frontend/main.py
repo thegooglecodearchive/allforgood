42710064b9aec37a7031acb0403d7904b790bb72
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-07-26 12:54:53 +0000

    ad change update

--- frontend/main.py
+++ frontend/main.py
@@ -17,7 +17,7 @@ appengine main().
 """
 
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
