e29db3149cdedda63ee55dc4d19b407ceb6c55dd
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2011-11-10 21:46:40 +0000

    handle bad spark feed

--- trunk/datahub/parse_sparked.py
+++ trunk/datahub/parse_sparked.py
@@ -59,13 +59,10 @@ import dateutil.parser
 def parse(instr, maxrec, progress):
   """return FPXML given sparked feed data"""
   from xml.dom import minidom
-  feed = minidom.parseString(instr.encode('utf-8'))
-  """
   try:
     feed = minidom.parseString(instr.encode('utf-8'))
   except:
     return None, 0, 0
-  """
 
   org_id = str(139)
   mission_statement = "Sparked makes it easy for people with busy lives to help nonprofits get valuable work done when it's convenient. We call it microvolunteering. Through the convenience of the Internet, and with the collaboration of others, micro-volunteers use their professional skills to help causes they care about."
