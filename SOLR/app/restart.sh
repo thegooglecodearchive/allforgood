339cb4ff381d7f30d2e03654d8080bea15b534d4
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2012-07-20 02:57:26 +0000

    hoc solr

--- SOLR/app/restart.sh
+++ SOLR/app/restart.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 :
 
-PID=`ps -ef | grep start.jar | grep -v grep | awk '{print $2}'`
-if [ ! "$PID" = "" ]
-then
-	echo killing $PID
-	kill -9 $PID
-fi
+while [ true ]
+do
+	PID=`ps -ef | grep start.jar | grep -v grep | awk '{print $2}' | head -1`
+	if [ "$PID" = "" ]
+	then
+		break
+	else
+		echo killing $PID
+		kill -9 $PID
+	fi
+done
 #exit
 
 cd /home/footprint/allforgood-read-only/SOLR/app
+#nohup /usr/bin/java -Dsolr.clustering.enabled=true -Xmx1024m -Xms1024m -jar start.jar >logs/solr.out 2>&1 &
 nohup /usr/bin/java -Xmx1024m -Xms1024m -jar start.jar >logs/solr.out 2>&1 &
 PID=`ps -ef | grep start.jar | grep -v grep | awk '{print $2}'`
 echo -n "pid=$PID "; date
