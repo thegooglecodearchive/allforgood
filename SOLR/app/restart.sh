6d4c41c7c5f0a6e7afe4112e34056112f06015d2
mt1955 <mt1955@3538cd10-6cae-11de-9a7c-6184c298a20a>
2012-07-26 13:46:09 +0000

    hoc

--- SOLR/app/restart.sh
+++ SOLR/app/restart.sh
@@ -14,8 +14,7 @@ do
 done
 #exit
 
-cd /home/footprint/allforgood-read-only/SOLR/app
-#nohup /usr/bin/java -Dsolr.clustering.enabled=true -Xmx1024m -Xms1024m -jar start.jar >logs/solr.out 2>&1 &
+cd /home/footprint/allforgood-read-only/SOLR/3app
 nohup /usr/bin/java -Xmx1024m -Xms1024m -jar start.jar >logs/solr.out 2>&1 &
 PID=`ps -ef | grep start.jar | grep -v grep | awk '{print $2}'`
 echo -n "pid=$PID "; date
