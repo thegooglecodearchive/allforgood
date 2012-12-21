27b42cf7ff0ef1893ecf99eb8afe5b48f0ad1d02
danstryker1@gmail.com <danstryker1@gmail.com@3538cd10-6cae-11de-9a7c-6184c298a20a>
2012-12-21 17:02:41 +0000

    MLK Day 2013 changes: revised category:mlkday algo, added MLK Day to categories, popular searches, and home page added POL promo unit

--- frontend/categories.py
+++ frontend/categories.py
@@ -17,7 +17,7 @@ A global list of categories for the SERP in the following format:
 "String used in Solr query" : "String displayed in SERP"
 """
 
-CATEGORIES = {  "military families":  "Military Families",     
+CATEGORIES = {  "(mlkday OR -2009 OR -2010 OR -2011 OR -2012 OR -feed_providername:girlscouts OR (-feed_providername:idealist AND eventrangestart:[0001-01-01T00:00:00Z TO 2013-01-27T23:59:59Z] AND eventrangeend:[2013-01-12T00:00:00Z TO *]))":  "MLK Day",     
                 "veterans":           "Veterans",        
                 "(education OR tutoring)":          "Education",
                 "hunger AND -animal": "Hunger",
